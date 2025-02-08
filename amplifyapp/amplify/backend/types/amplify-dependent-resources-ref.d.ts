7ba73dd0d8e0201bf076e185479cc6d349d840cc
Takafumi Fukai <takyafumin@gmail.com>
2025-02-08 15:27:44 +0900

    add, cognito

--- amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyapp95a5f4d8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
